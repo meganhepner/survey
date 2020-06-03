95956d19c76510f3c19619ab1b653ea12a750114
Megan Hepner <megan.hepner@gmail.com>
2020-06-03 16:34:04 -0700

    start select boxes

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,11 @@ $(document).ready(function() {
         event.preventDefault();
         const name = $("input#name").val();
         const flavor = $("input:radio[name=flavor]:checked").val();
+        const music = $("#music").val();
+        
         $(".food").text(flavor);
         $(".name").text(name);
+        $(".music").text(music);
     });
-
-    // $("form#name").submit(function(event) {
-    //     event.preventDefault();
-
-    // });
+ });
 });
